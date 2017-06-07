f45c4dd4203594e0f761440812212b5857ad972a
Mark Rackley <mrackley@gmail.com>
2017-06-07 13:43:42 -0500

    1.1 Updates
    My good friend Waldek Mastykarz made a couple of updates to show
    different colors for tasks depending on who the task is assigned to as
    well as some style changes per his stile of coding. Additionally he
    changed the task update to update both the start date and due date when
    a task is dragged and dropped to keep the duration the same.

--- SPFullCalendar.js
+++ SPFullCalendar.js
@@ -1,6 +1,7 @@
+<!--
 /*
  * SPFullCalendar - Create a calendar view using FullCalendar.io and SharePoint Online Task list
- * Version 1.0 
+ * Version 1.1 
  * @requires jQuery v1.11 or greater 
  * @requires jQuery, FullCalendar.io, Moment.js 
  *
@@ -15,120 +16,135 @@
  * @category Plugins/SPFullCalendar
  * @author Mark Rackley / http://www.paitgroup.com / mrackley@paitgroup.com
  */
+-->
 
-<script type="text/javascript" src="//code.jquery.com/jquery-1.11.1.min.js"></script> 
-<script  type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+<link type="text/css" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.css" />
+<div id="calendar"></div>
 
-<script  type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
-<link  type="text/css" rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.css" /> 
-
-<div id='calendar'></div>
-
-<script type="text/javascript">
-
-  var PATH_TO_DISPFORM = "https://<url to site>/Lists/Tasks/DispForm.aspx";
+<script>
+  var PATH_TO_DISPFORM = _spPageContextInfo.webAbsoluteUrl + "/Lists/Tasks/DispForm.aspx";
   var TASK_LIST = "Tasks";
+  var COLORS = ['#466365', '#B49A67', '#93B7BE', '#E07A5F', '#849483', '#084C61', '#DB3A34'];
 
-   DisplayTasks();
-   
-   function DisplayTasks()
-   {
-  	$('#calendar').fullCalendar( 'destroy' );
+  displayTasks();
+
+  function displayTasks() {
+    $('#calendar').fullCalendar('destroy');
     $('#calendar').fullCalendar({
+      weekends: false,
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,basicWeek,basicDay'
+      },
+      // open up the display form when a user clicks on an event
+      eventClick: function (calEvent, jsEvent, view) {
+        window.location = PATH_TO_DISPFORM + "?ID=" + calEvent.id;
+      },
+      editable: true,
+      timezone: "UTC",
+      droppable: true, // this allows things to be dropped onto the calendar
+      // update the end date when a user drags and drops an event 
+      eventDrop: function (event, delta, revertFunc) {
+        updateTask(event.id, event.start, event.end);
+      },
+      // put the events on the calendar 
+      events: function (start, end, timezone, callback) {
+        var startDate = start.format('YYYY-MM-DD');
+        var endDate = end.format('YYYY-MM-DD');
 
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,basicWeek,basicDay'
-        },
-        //open up the display form when a user clicks on an event
-        eventClick: function (calEvent, jsEvent, view) {
-			    window.location = PATH_TO_DISPFORM + "?ID=" + calEvent.id;
-        },
-       	editable: true,
-        timezone: "UTC",
-        droppable: true, // this allows things to be dropped onto the calendar
-        //update the end date when a user drags and drops an event 
-        eventDrop: function(event, delta, revertFunc) {
-            UpdateTask(event.id,event.end);
-          }, 
-        //put the events on the calendar 
-        events: function (start, end, timezone, callback) {
-            startDate = start.format('YYYY-MM-DD');
-            endDate = end.format('YYYY-MM-DD');
-			
-			var RESTQuery = "/_api/Web/Lists/GetByTitle('"+TASK_LIST+"')/items?$select=ID,Title,\
-				Status,StartDate,DueDate,AssignedTo/Title&$expand=AssignedTo&\
-				$filter=((DueDate ge '"+startDate+"' and DueDate le '"+endDate+"'))";
-			
-			var opencall = $.ajax({
-		    		url: _spPageContextInfo.webAbsoluteUrl + RESTQuery,
-		    		type: "GET",
-		    		dataType: "json",
-		    		headers: {
-		    			Accept: "application/json;odata=verbose"
-		    		}
-		    });
+        var restQuery = "/_api/Web/Lists/GetByTitle('" + TASK_LIST + "')/items?$select=ID,Title,\
+Status,StartDate,DueDate,AssignedTo/Title&$expand=AssignedTo&\
+$filter=((DueDate ge '" + startDate + "' and DueDate le '" + endDate + "')or(StartDate ge '" + startDate + "' and StartDate le '" + endDate + "'))";
 
-            opencall.done(function (data, textStatus, jqXHR) {
-			    	var events = [];
-		    		for (index in data.d.results)
-		    		{
-						var assignedTo = "";
-						for(person in data.d.results[index].AssignedTo.results)
-						{
-							assignedTo += data.d.results[index].AssignedTo.results[person].Title + " ";
-						}
-						events.push({
-									title: data.d.results[index].Title + " - " + assignedTo,
-									id: data.d.results[index].ID,
-                  color: "#c00000", //specify the background color and border color can also create a class and use className paramter. 
-									start: moment.utc(data.d.results[index].StartDate),
-									end: moment.utc(data.d.results[index].DueDate).add("1","days") //add one day to end date so that calendar properly shows event ending on that day
-								});
-		    		}
-					
-					callback(events);
-            
-		    });
-		}
-	});
-}
+        $.ajax({
+          url: _spPageContextInfo.webAbsoluteUrl + restQuery,
+          type: "GET",
+          dataType: "json",
+          headers: {
+            Accept: "application/json;odata=nometadata"
+          }
+        })
+          .done(function (data, textStatus, jqXHR) {
+            var events = [];
+            var personColors = {};
+            var colorNo = 0;
 
+            var events = data.value.map(function (task) {
+              var assignedTo = task.AssignedTo.map(function (person) {
+                return person.Title;
+              }).join(', ');
 
-function UpdateTask(id,dueDate)
-{
-      //substract the previoulsy added day to the date to store correct date
-			sDate =  moment.utc(dueDate).add("-1","days").format('YYYY-MM-DD') + "T" + 
-					dueDate.format("hh:mm")+ ":00Z";
-					
-        var call = jQuery.ajax({
-	        url: _spPageContextInfo.webAbsoluteUrl +
-	            "/_api/Web/Lists/getByTitle('"+TASK_LIST+"')/Items(" + id + ")",
-	        type: "POST",
-	        data: JSON.stringify({
-	            "__metadata": { type: "SP.Data.TasksListItem" },
-	            DueDate: sDate,
-	        }),
-	        headers: {
-	            Accept: "application/json;odata=verbose",
-	            "Content-Type": "application/json;odata=verbose",
-	            "X-RequestDigest": jQuery("#__REQUESTDIGEST").val(),
-	            "IF-MATCH": "*",
-	            "X-Http-Method": "PATCH"
-	        }
+              var color = personColors[assignedTo];
+              if (!color) {
+                color = COLORS[colorNo++];
+                personColors[assignedTo] = color;
+              }
+              if (colorNo >= COLORS.length) {
+                colorNo = 0;
+              }
+
+              return {
+                title: task.Title + " - " + assignedTo,
+                id: task.ID,
+                color: color, // specify the background color and border color can also create a class and use className paramter. 
+                start: moment.utc(task.StartDate).add("1", "days"),
+                end: moment.utc(task.DueDate).add("1", "days") // add one day to end date so that calendar properly shows event ending on that day
+              };
+            });
+
+            callback(events);
+          });
+      }
+    });
+  }
+
+  function updateTask(id, startDate, dueDate) {
+    // subtract the previously added day to the date to store correct date
+    var sDate = moment.utc(startDate).add("-1", "days").format('YYYY-MM-DD') + "T" +
+      startDate.format("hh:mm") + ":00Z";
+    if (!dueDate) {
+      dueDate = startDate;
+    }
+    var dDate = moment.utc(dueDate).add("-1", "days").format('YYYY-MM-DD') + "T" +
+      dueDate.format("hh:mm") + ":00Z";
+
+    $.ajax({
+      url: _spPageContextInfo.webAbsoluteUrl + '/_api/contextinfo',
+      type: 'POST',
+      headers: {
+        'Accept': 'application/json;odata=nometadata'
+      }
+    })
+      .then(function (data, textStatus, jqXHR) {
+        return $.ajax({
+          url: _spPageContextInfo.webAbsoluteUrl +
+          "/_api/Web/Lists/getByTitle('" + TASK_LIST + "')/Items(" + id + ")",
+          type: 'POST',
+          data: JSON.stringify({
+            StartDate: sDate,
+            DueDate: dDate,
+          }),
+          headers: {
+            Accept: "application/json;odata=nometadata",
+            "Content-Type": "application/json;odata=nometadata",
+            "X-RequestDigest": data.FormDigestValue,
+            "IF-MATCH": "*",
+            "X-Http-Method": "PATCH"
+          }
         });
-        call.done(function (data, textStatus, jqXHR) {
-        	alert("Update Successful");
-          DisplayTasks();
-        });
-        call.fail(function (jqXHR, textStatus, errorThrown) {
-           alert("Update Failed");
-           DisplayTasks();
-        });
-
-}
-
-
-
+      })
+      .done(function (data, textStatus, jqXHR) {
+        alert("Update Successful");
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        alert("Update Failed");
+      })
+      .always(function () {
+        displayTasks();
+      });
+  }
 </script>
